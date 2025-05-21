80ef2992ba5f9a308f17ee50de8f10a555a92607
El Mehdi Laidoudi <elmehdilaidoudi@gmail.com>
2025-05-21 07:07:12 +0100

    refactoring: update imports

--- web/src/routes/_layout/(public)/sign-up/index.tsx
+++ web/src/routes/_layout/(public)/sign-up/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import SignUp from '../../../../modules/signup/index.tsx'
+import SignUp from '@/modules/auth/components/signup'
 
 export const Route = createFileRoute('/_layout/(public)/sign-up/')({
   component: SignUp,
