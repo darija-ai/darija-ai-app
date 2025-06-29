967001e8dbbd75e3ec4ef5455718f03b728d4b86
Ikram Ghala <ikram.ghala120@gmail.com>
2025-06-29 18:44:40 +0100

    fixing navbar routes

--- web/src/shared/navigation/public-navigation/top-nav/navbar.tsx
+++ web/src/shared/navigation/public-navigation/top-nav/navbar.tsx
@@ -1,30 +1,39 @@
-import { Button } from "@/shared/components/ui/button"
-import { NavMenu } from "./nav-menu"
-import { NavigationSheet } from "./navigation-sheet"
+import { Button } from "@/shared/components/ui/button";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "./navigation-sheet";
+import { useNavigate } from "@tanstack/react-router";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className="fixed top-6 inset-x-4 h-16 bg-black/20 backdrop-blur-md border border-white/10 max-w-7xl mx-auto rounded-full z-50">
       <div className="h-full flex items-center justify-between mx-auto px-4">
         <div className="text-white ml-4">
-          <img 
+          <img
             src="/logo.webp"
             width={60}
-            className="invert"
+            className="invert hover:opacity-50"
+            onClick={() => navigate({ to: "/" })}
           />
         </div>
 
-        {/* Desktop Menu */}
         <NavMenu className="hidden md:block text-white" />
 
         <div className="flex items-center gap-3">
           <Button
             variant="ghost"
             className="hidden sm:inline-flex rounded-full bg-transparent text-white  border-white/20 hover:bg-white/10 hover:text-white"
+            onClick={() => navigate({ to: "/sign-in" })}
           >
             Login
           </Button>
-          <Button className="rounded-full bg-white text-black hover:bg-amber-100">Sign Up</Button>
+          <Button
+            className="rounded-full bg-white text-black hover:bg-amber-100"
+            onClick={() => navigate({ to: "/sign-up" })}
+          >
+            Sign Up
+          </Button>
 
           {/* Mobile Menu */}
           <div className="md:hidden">
@@ -33,5 +42,5 @@ export const Navbar = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
