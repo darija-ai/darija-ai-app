a90395fd2fe381369ca0f71ad648c7b64c4250cb
El mehdi Laidoudi <75626259+binarymehdi@users.noreply.github.com>
2025-09-09 10:02:58 +0000

    feat: write docker compose configuration for db, server, & web

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,9 +9,9 @@ import apiRouter from './routes/api';
 dotenv.config();
 
 const config = {
-  port: parseInt(process.env.PORT || '3000', 10),
-  frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3001',
-  nodeEnv: process.env.NODE_ENV || 'development',
+  port: parseInt(process.env.PORT || '899', 10),
+  frontendUrl: process.env.FRONTEND_URL,
+  nodeEnv: process.env.NODE_ENV || 'development1',
 };
 
 const app = express();
